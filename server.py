a6d725a0e70cd877ab4b5244d754b01d50f8aa66
Harnet69 <harnet69@gmail.com>
2019-11-06 08:51:20 +0100

    Generation user story id was implemented

--- server.py
+++ server.py
@@ -5,20 +5,29 @@ import data_handler
 app = Flask(__name__)
 
 
+# index page for
 @app.route('/')
 def route_index():
-    stories_list = data_handler.get_data_from_file()
-
+    stories_list = data_handler.get_data_from_file()  # list of user stories
     return render_template('index.html', stories_headers=data_handler.DATA_HEADER, user_stories=stories_list)
 
 
+# add new user story
 @app.route('/story', methods=['GET', 'POST'])
 def route_story():
     if request.method == 'POST':
         user_data_input = request.form  # get data from form to dictionary
-
+        stories_list = data_handler.get_data_from_file()  # list of user stories
+        if stories_list:
+            max_id = max(stories_list)
+            try:
+                new_story_id = int(max_id[0]) + 1
+            except ValueError:
+                print("It's not a number")
+        else:
+            new_story_id = 0
         user_store_data = []
-        user_store_data.append("Id")  # TODO add appropriate function
+        user_store_data.append(new_story_id)  # TODO add appropriate function
         user_store_data.append(user_data_input["story_title"])
         user_store_data.append(user_data_input["user_story"])
         user_store_data.append(user_data_input["acc_crit"])
@@ -31,6 +40,13 @@ def route_story():
 
     else:
         return render_template('story.html', statuses=data_handler.STATUSES)
+
+
+# edit user story
+@app.route('/story/<int:post_id>', methods=['GET', 'POST'])
+def route_edit_story(post_id):
+    # get stories from file, find one by id and send a list with values to template for displaying as values for change
+    return render_template('story.html', statuses=data_handler.STATUSES)
 
 
 if __name__ == '__main__':
