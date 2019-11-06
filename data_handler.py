f5eaf54d0cf0846ae862721ad972821f8b415d7b
Harnet69 <harnet69@gmail.com>
2019-11-06 14:33:24 +0100

    Update works perfect

--- data_handler.py
+++ data_handler.py
@@ -5,7 +5,7 @@ DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ e
 DATA_HEADER = ['id', 'title', 'user_story', 'acceptance_criteria', 'business_value', 'estimation', 'status']
 STATUSES = ['planning', 'todo', 'in progress', 'review', 'done']
 
-
+# get user stories from file as a list of lists
 def get_data_from_file():
     try:
         with open(DATA_FILE_PATH, newline='') as f:
@@ -17,15 +17,15 @@ def get_data_from_file():
     except FileNotFoundError as e:
         print(e)
 
-
+# write(append) a new story/ rewrite updated stories if story was updated
 def write_data_to_file(user_data, write_one_row='True'):
     try:
         if write_one_row:
-            with open(DATA_FILE_PATH, 'a') as f:
+            with open(DATA_FILE_PATH, 'a') as f:  # append a new
                 schedule_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
                 schedule_writer.writerow(user_data)
         else:
-            with open(DATA_FILE_PATH, 'w') as f:
+            with open(DATA_FILE_PATH, 'w') as f:  # rewrite all stories
                 schedule_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
                 schedule_writer.writerows(user_data)
     except FileNotFoundError as e:
--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import data_handler
 app = Flask(__name__)
 
 
-# index page for
+# index page
 @app.route('/')
 def route_index():
     stories_list = data_handler.get_data_from_file()  # list of user stories
@@ -15,19 +15,19 @@ def route_index():
 # add new user story
 @app.route('/story', methods=['GET', 'POST'])
 def route_story():
-    if request.method == 'POST':
-        user_data_input = request.form  # get data from form to dictionary
+    if request.method == 'POST':  # if user submit new story by a form
+        user_data_input = request.form  # got data from form
         stories_list = data_handler.get_data_from_file()  # list of user stories
         if stories_list:
-            max_id = max(stories_list)
+            max_id = max(stories_list)  # get the last story id
             try:
-                new_story_id = int(max_id[0]) + 1
+                new_story_id = int(max_id[0]) + 1  # give to new user story the next id
             except ValueError:
                 print("It's not a number")
         else:
-            new_story_id = 0
+            new_story_id = 0  #  if it's the first story
         user_store_data = []
-        user_store_data.append(new_story_id)  # TODO add appropriate function
+        user_store_data.append(new_story_id)
         user_store_data.append(user_data_input["story_title"])
         user_store_data.append(user_data_input["user_story"])
         user_store_data.append(user_data_input["acc_crit"])
@@ -35,19 +35,41 @@ def route_story():
         user_store_data.append(user_data_input["estimation"])
         user_store_data.append(user_data_input["status"])
 
-        data_handler.write_data_to_file(user_store_data)
+        data_handler.write_data_to_file(user_store_data)  # write the story to a file
         return redirect('/')
 
     else:
-        return render_template('story.html', statuses=data_handler.STATUSES)
+        return render_template('story.html', statuses=data_handler.STATUSES)  # if a request method if GET
 
 
 # edit user story
 @app.route('/story/<int:post_id>', methods=['GET', 'POST'])
 def route_edit_story(post_id):
+    user_data_input = request.form  # get data from form to dictionary
     stories_list = data_handler.get_data_from_file()  # list of user stories
     story_to_edit = [x for x in stories_list if str(x[0]) == str(post_id)]  # find the story by id
-    if story_to_edit:
+
+    if request.method == "POST":
+        updated_story = []  # updated story from form
+        updated_story.append(user_data_input["story_id"])
+        updated_story.append(user_data_input["story_title"])
+        updated_story.append(user_data_input["user_story"])
+        updated_story.append(user_data_input["acc_crit"])
+        updated_story.append(user_data_input["business_value"])
+        updated_story.append(user_data_input["estimation"])
+        updated_story.append(user_data_input["status"])
+
+        updated_stories = [] # updated by user stories
+        for story in stories_list:
+            if story[0] == updated_story[0]:  # if it's the updated story
+                updated_stories.append(updated_story)
+            else:
+                updated_stories.append(story)
+
+        data_handler.write_data_to_file(updated_stories, False)  # rewrite stories file by ONE TRANSACTION
+        return redirect('/')
+
+    if story_to_edit:  # check if typed by user in a browser line story id exists
         return render_template('story.html', statuses=data_handler.STATUSES, story_to_edit=story_to_edit[0])
     else:
         error_message = f"Store with id = {post_id} doesn't exist!"
